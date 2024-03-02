a90745aa6dd9633e93309850ce22a12e1430bc01
chrislpoy <chrislpoy@gmail.com>
2024-03-02 18:05:00 -0400

    first ever ruby program

---
+++ greetings.rb
@@ -0,0 +1,8 @@
+# Prompt the user to enter their name
+puts "What's your name?"
+
+# Get the user's input and store it in a variable
+name = gets.chomp
+
+# Greet the user
+puts "Hello, #{name}! Welcome to the world of Ruby programming."
